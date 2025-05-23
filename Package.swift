7f3cb1f05487cb4ef11d0b76172503015ac2be95
Jim Raden <james.raden@gmail.com>
2025-05-23 15:45:44 -0400

    *TEMP* Get rid of test targets in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftCheckSpam",
     platforms: [
-        .macOS(.v12) // Requires macOS 12+ for async/await and other modern Swift features
+        .macOS(.v12)  // Requires macOS 12+ for async/await and other modern Swift features
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
         // For a robust local HTTP server for OAuth, consider a library like:
         // .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0") // Example: Swifter
         // .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0") // For HTTP client needs beyond URLSession
@@ -18,11 +18,11 @@ let package = Package(
         .executableTarget(
             name: "SwiftCheckSpam",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
                 // If using Swifter: .product(name: "Swifter", package: "swifter"),
-            ]),
-        .testTarget(
-            name: "SwiftCheckSpamTests",
-            dependencies: ["SwiftCheckSpam"]),
+            ])
+        // .testTarget(
+        //     name: "SwiftCheckSpamTests",
+        //     dependencies: ["SwiftCheckSpam"]),
     ]
 )
