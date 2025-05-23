e8c290785bd9b631bd38e0c7def8626a0acd65f6
Jim Raden <james.raden@gmail.com>
2025-05-23 18:32:35 -0400

    Regenerate using `swift package init --type tool --name SwiftCheckSpam`

--- Sources/SwiftCheckSpam/SwiftCheckSpam.swift
+++ Sources/SwiftCheckSpam/SwiftCheckSpam.swift
@@ -1,3 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+//
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
 import ArgumentParser
 import Foundation
 
