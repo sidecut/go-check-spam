cd06ad6a1d26cca3ab5a0e1a5586d2036edb5c35
Jim Raden <james.raden@gmail.com>
2025-07-10 13:51:28 -0400

    Parameterize initial delay for message fetching to avoid hitting rate limits

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 )
 
 var timeout = flag.Int("timeout", 60, "timeout in seconds")
+var initialDelay = flag.Int("initial-delay", 1000, "max initial delay in milliseconds before starting to fetch messages")
 var days = flag.Int("days", 30, "number of days to look back")
 var debug = flag.Bool("debug", false, "enable debug output")
 var cutoffDate string
@@ -113,8 +114,8 @@ func listSpamMessages(ctx context.Context, srv *gmail.Service) ([]*gmail.Message
 			go func(messageId string) {
 				defer wg.Done()
 
-				// delay a random interval between 0 and 100 milliseconds to avoid hitting rate limits
-				time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+				// delay a random interval between 0 and initialDelay milliseconds to avoid hitting rate limits
+				time.Sleep(time.Duration(rand.Intn(*initialDelay)) * time.Millisecond)
 
 				fullMsg, err := backoff.Retry(ctx, func() (*gmail.Message, error) {
 					// Fetch the full message using exponential backoff
